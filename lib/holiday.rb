f8bb812320acccdf992b879d08246bd5c5c6b9d5
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-25 18:04:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
      holiday.each do |holiday, item|
        puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
-  end 
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -72,6 +72,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
    holiday.collect do |holiday, item|
      holiday if item.include?("BBQ")
-   end 
+   end
  end.flatten.compact
 end
